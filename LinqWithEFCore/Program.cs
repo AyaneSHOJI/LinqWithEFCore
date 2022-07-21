7545af4fdf9792ff7f9c4337107c57c1641d9351
AyaneSHOJI <shojiayane@gmail.com>
2022-07-21 23:45:17 +0200

    group-joining sequences

--- LinqWithEFCore/Program.cs
+++ LinqWithEFCore/Program.cs
@@ -2,8 +2,9 @@
 using Packt.Shared;
 using Microsoft.EntityFrameworkCore;
 
-FilterAndSort();
-JoinCategoriesAndProducts();
+//FilterAndSort();
+//JoinCategoriesAndProducts();
+GroupJoinCategoriesAndProducts();
 
 static void FilterAndSort()
 {
@@ -52,6 +53,35 @@ static void JoinCategoriesAndProducts()
                 arg0: item.ProductId,
                 arg1: item.ProductName,
                 arg2: item.CategoryName);
+        }
+    }
+}
+
+static void GroupJoinCategoriesAndProducts()
+{
+    using(Northwind db = new())
+    {
+        // group all products by their category to return 8 matches
+        var queryGroup = db.Categories.AsEnumerable().GroupJoin(
+            inner: db.Products,
+            outerKeySelector: category => category.CategoryId,
+            innerKeySelector: product => product.CategoryId,
+            resultSelector: (c, matchingProducts) => new
+            {
+                c.CategoryName,
+                Products = matchingProducts.OrderBy(p => p.ProductName)
+            });
+
+        foreach(var category in queryGroup)
+        {
+            WriteLine("{0}: has {1} products",
+                arg0: category.CategoryName,
+                arg1: category.Products.Count());
+
+            foreach(var product in category.Products)
+            {
+                WriteLine($"  {product.ProductName}");
+            }
         }
     }
 }
