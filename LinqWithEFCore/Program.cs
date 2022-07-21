73c0d95062491ea0c82ccf0fbf336d2dda342976
AyaneSHOJI <shojiayane@gmail.com>
2022-07-21 23:27:13 +0200

    project sequences in new type

--- LinqWithEFCore/Program.cs
+++ LinqWithEFCore/Program.cs
@@ -9,15 +9,21 @@ static void FilterAndSort()
     using(Northwind db = new())
     {
         DbSet<Product> allProducts = db.Products;
-        //IQueryable<Product> filteredProducts = allProducts.Where(product => product.UnitPrice < 10M);
-        //// IOrderedQueryable : Represents the result of a sorting operation.
-        //IOrderedQueryable<Product> sortedAndFilteredProducts = filteredProducts.OrderByDescending(product => product.UnitPrice);
-    
+        IQueryable<Product> filteredProducts = allProducts.Where(product => product.UnitPrice < 10M);
         // IOrderedQueryable : Represents the result of a sorting operation.
-        IQueryable<Product> sortedAndFilteredProducts = allProducts.Where(product => product.UnitPrice < 10M).OrderByDescending(product => product.UnitPrice);
+        IOrderedQueryable<Product> sortedAndFilteredProducts = filteredProducts.OrderByDescending(product => product.UnitPrice);
+
+        // anonymous type
+        var projectedProducts = sortedAndFilteredProducts.
+            Select(product => new
+            {
+                product.ProductId,
+                product.ProductName,
+                product.UnitPrice
+            });
 
         WriteLine("Products that cost less than $10:");
-        foreach(Product p in sortedAndFilteredProducts)
+        foreach(var p in projectedProducts)
         {
             WriteLine("{0}:{1} costs {2:$#,##0.00}", 
                 p.ProductId, p.ProductName, p.UnitPrice);
